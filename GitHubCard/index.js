516b7c8ffa7b957044752ea83be9f8558d5faaae
toddmurphy <toddmurphy93@gmail.com>
2019-10-31 20:21:25 -0300

    deleted some comments

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -43,21 +43,6 @@ function githubCard(data) {
   const githubBio = document.createElement('p');
   const githubImg = document.createElement('img');
 
-  //   <div class="card">
-  //   <img src={image url of user} />
-  //   <div class="card-info">
-  //     <h3 class="name">{users name}</h3>
-  //     <p class="username">{users user name}</p>
-  //     <p>Location: {users location}</p>
-  //     <p>Profile:
-  //       <a href={address to users github page}>{address to users github page}</a>
-  //     </p>
-  //     <p>Followers: {users followers count}</p>
-  //     <p>Following: {users following count}</p>
-  //     <p>Bio: {users bio}</p>
-  //   </div>
-  // </div>
-
   //Set up structure of elements
   card.appendChild(githubImg);
   card.appendChild(cardContainer);
