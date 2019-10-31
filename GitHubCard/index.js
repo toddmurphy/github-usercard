cd3bb0d395b2bf41b3dc92e0e6132526cb0c1098
toddmurphy <toddmurphy93@gmail.com>
2019-10-31 16:16:14 -0300

    got github personal profile data request working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios
+  .get('https://api.github.com/users/toddmurphy')
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log('Your response failed and data not returning', error);
+  });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
